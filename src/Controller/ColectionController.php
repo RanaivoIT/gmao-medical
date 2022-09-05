556385f51d9ae300db05ad0b7f9f9026a7935853
ranaivo.it <ranaivo.it@gmail.com>
2022-09-06 00:16:42 +0300

    Maj AdministrateurController

--- src/Controller/ColectionController.php
+++ src/Controller/ColectionController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ColectionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ColectionController extends AbstractController
 {
-    #[Route('/colection', name: 'app_colection')]
-    public function index(): Response
+    #[Route('/administrateur/collections', name: 'administrateur_colection_list')]
+    public function list(ColectionRepository $repo): Response
     {
+        $colevtions = $repo->findAll();
         return $this->render('colection/index.html.twig', [
             'controller_name' => 'ColectionController',
         ]);
--- src/Controller/TechnicienController.php
+++ src/Controller/TechnicienController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\TechnicienRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TechnicienController extends AbstractController
 {
-    #[Route('/technicien', name: 'app_technicien')]
-    public function index(): Response
+    #[Route('/technicien/techniciens', name: 'technicien_list')]
+    public function index(TechnicienRepository $repo): Response
     {
+        $techniciens = $repo->findAll();
         return $this->render('technicien/index.html.twig', [
             'controller_name' => 'TechnicienController',
         ]);
